d63c4a54f4637712720a63f9263dac992246b863
Yaoni <wyatsky@gmail.com>
2020-07-03 16:16:21 +1000

    Add tests for cycling_treatments.

--- cycling_treatments/tests.py
+++ cycling_treatments/tests.py
@@ -1,9 +1,20 @@
-from otree.api import Currency as c, currency_range
+from otree.api import Currency as c, currency_range, expect
 from . import pages
 from ._builtin import Bot
-from .models import Constants
+from .models import Constants, Treatment
 
 
 class PlayerBot(Bot):
     def play_round(self):
-        pass
+
+        yield pages.MyPage
+
+        print(self.participant.id_in_session)
+        if self.participant.id_in_session == 1:
+            expect(self.player.treatment, "==", Treatment.T1.name)
+        if self.participant.id_in_session == 2:
+            expect(self.player.treatment, "==", Treatment.T2.name)
+        if self.participant.id_in_session == 3:
+            expect(self.player.treatment, "==", Treatment.T3.name)
+        if self.participant.id_in_session == 4:
+            expect(self.player.treatment, "==", Treatment.T4.name)

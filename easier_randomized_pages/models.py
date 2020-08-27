11aab87d400b77b4961a63720a7d4b956ea97c54
Yaoni <wyatsky@gmail.com>
2020-08-27 15:06:03 +1000

    Misc: add more comments on code.

--- easier_randomized_pages/models.py
+++ easier_randomized_pages/models.py
@@ -21,10 +21,11 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'easier_randomized_pages'
     players_per_group = None
+    # set this to the number of pages that are to be randomized
     num_rounds = 3
 
-    # this has to be preset
-    # but creating this list should be fairly easy
+    # This does not need to be a list,
+    # Creating this list should be fairly easy
     page_masks = [
         "001",
         "010",
@@ -35,6 +36,8 @@ class Constants(BaseConstants):
 class Subsession(BaseSubsession):
     def creating_session(self):
         for p in self.get_players():
+            # player.page_mask should be the same across all rounds
+            # every player will randomly get a page_mask
             if self.round_number == 1:
                 page_masks = Constants.page_masks.copy()
                 shuffle(page_masks)
@@ -42,6 +45,9 @@ class Subsession(BaseSubsession):
             else:
                 p.page_mask = p.in_round(1).page_order
 
+            # at the start of each round
+            # we get the page mask for the round
+            # e.g. in round 2, we get the 010 part in 100010001
             p.page_mask_in_round = p.page_order[
                 Constants.num_rounds
                 * (self.round_number - 1) : Constants.num_rounds
@@ -54,5 +60,14 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    # the page mask for all rounds
+    # for a 3-round (3-page) app
+    # this would be something like "001100010"
+    # this will be the same across all rounds
     page_mask = models.StringField()
+
+    # the page mask for a single round
+    # for a 3-round (3-page) app
+    # this would be something like "001"
+    # this will be different across all rounds
     page_mask_in_round = models.StringField()
--- easier_randomized_pages/pages.py
+++ easier_randomized_pages/pages.py
@@ -4,12 +4,21 @@ from .models import Constants
 
 
 class ControlPage(Page):
+    """
+    Use this page class to control the diplay of its subclass.
+    """
+
     def is_displayed(self):
+        # for page P_1, we get number 1
         cur_page_num = int(self.__class__.__name__.split('_')[1])
 
-        return cur_page_num // cur_page_num & int(
-            self.player.page_mask_in_round[cur_page_num - 1]
-        )
+        # say player's page_mask_in_round is "001"
+        # then only when cur_page_num == 3 will we get a 1 from 
+        # self.player.page_mask_in_round[cur_page_num - 1]
+        # Otherwise we only get 0.
+        # Adding 1 && (bitwise and) is just me personally want to explicitly
+        # return True or False, instead of 1 or 0.
+        return 1 && int(self.player.page_mask_in_round[cur_page_num - 1])
 
 
 class P_1(ControlPage):
